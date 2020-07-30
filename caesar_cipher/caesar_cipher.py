e86daf08240a88dddf596dd948b8946f875a96ad
vorSherer <tcpma2011@gmail.com>
2020-07-29 20:16:44 -0700

    Preliminary README scaffold, code and PoL tests.

---
+++ caesar_cipher/caesar_cipher.py
@@ -0,0 +1,21 @@
+def encrypt(plain_text, key):
+    """
+    docstring here
+    """
+    
+def decrypt(encrypted_text, key):
+    """
+    docstring here
+    """
+
+def shifted(key):
+    """
+    docstring here
+    """
+
+
+
+
+if __name__ == "__main__":
+    pass
+
