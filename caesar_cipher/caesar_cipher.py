b0dc30ce4bf152bdfaa7b81597a414dff26cc390
vorSherer <tcpma2011@gmail.com>
2020-07-29 20:18:04 -0700

    Added 'pass' to functions.

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -2,16 +2,19 @@ def encrypt(plain_text, key):
     """
     docstring here
     """
+    pass
     
 def decrypt(encrypted_text, key):
     """
     docstring here
     """
+    pass
 
 def shifted(key):
     """
     docstring here
     """
+    pass
 
 
 
