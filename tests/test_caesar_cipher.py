61cf36c4d414e16743c6804f2407139c91455fef
vorSherer <tcpma2011@gmail.com>
2020-07-30 22:45:40 -0700

    Added working tests for all functions.

--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -4,7 +4,7 @@ from caesar_cipher import __version__
 from caesar_cipher.caesar_cipher import (
     encrypt,
     decrypt,
-    # shifted,
+    caesar_cracker,
 )
 
 
@@ -20,8 +20,24 @@ def test_decrypt_exists():
     assert decrypt
 
 
-@pytest.mark.skip
-def test_shifted_exists():
-    assert shifted
+def test_caesar_cracker_exists():
+    assert caesar_cracker
 
+
+def test_encrypt_passes():
+    expected = "Udymts nx xsjfpd"
+    actual = encrypt("Python is sneaky", 5)
+    assert actual == expected
+
+
+def test_decrypt_passes():
+    expected = "Python is sneaky"
+    actual = decrypt("Udymts nx xsjfpd", 5)
+    assert actual == expected
+
+
+# @pytest.mark.skip
+def test_caesar_cracker_passes():
+    expected = "It was the best of times, it was the worst of times."
+    actual = caesar_cracker('Xi lph iwt q...ghi du ixbth.')
 
